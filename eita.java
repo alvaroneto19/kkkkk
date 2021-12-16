3cf6822fb51e5c16d8c213eb0ed9bd6c3e9a4a86
alvaroneto19 <81449169+alvaroneto19@users.noreply.github.com>
2021-12-15 23:42:02 -0300

    Create eita.java

---
+++ eita.java
@@ -0,0 +1,28 @@
+pacote  poo ;
+
+public  class  Cliente {
+     nome da string privada , cpf;
+
+    public  Cliente ( String  nome , String  cpf ) {
+        isso . nome = nome;
+        isso . cpf = cpf;
+    }
+
+    public  String  getNome () {
+        devolva  isso . nome;
+    }
+
+    public  void  setNome ( String  nome ) {
+        isso . nome = nome;
+    }
+    
+    public  String  getCpf () {
+        devolva  isso . cpf;
+    }
+
+    public  void  setCpf ( String  cpf ) {
+        isso . cpf = cpf;
+    }
+
+
+}
